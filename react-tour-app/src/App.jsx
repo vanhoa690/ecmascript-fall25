2f8d6d79a69e0a540df5e306d790a8651df919c4
vanhoa690 <vanhoa690@gmail.com>
2025-11-12 15:47:35 +0700

    export import

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -5,11 +5,17 @@ import './App.css'
 
 function App() {
   const greeting = 'Chào mừng đến với React!'
-
+  const name = 'Nguyễn Văn A'
+  const age = 20
   return (
     <div className="container">
       <h1>{greeting}</h1>
       <p>Đây là component đầu tiên của bạn</p>
+      <h1>Xin chào {name}</h1>
+      <p>Tuổi: {age + 1}</p>
+      <p>Là học viên: {true ? 'Có' : 'Không'}</p>
+      <button>Xem chi tiet tour</button>
+      {/* Tour Card */}
     </div>
   )
 }
---
+++ utils.js
@@ -0,0 +1,8 @@
+export const PI = 3.14
+export function sum(a, b) {
+  return a + b
+}
+
+export default function multiply(a, b) {
+  return a * b
+}
