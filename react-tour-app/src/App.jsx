49a84a2db7629e62a5f789530f920c3b5a3d6864
vanhoa690 <vanhoa690@gmail.com>
2025-11-14 15:15:55 +0700

    map list tours

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -1,4 +1,31 @@
 function App() {
+  // mock data
+  const tours = [
+    {
+      id: 1,
+      title: 'Tour Du Lịch Cuba',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+    {
+      id: 2,
+      title: 'Tour Du Lịch Hoa Ky',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+    {
+      id: 3,
+      title: 'Tour Du Lịch Nhat Ban Update',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
+    },
+    {
+      id: 4,
+      title: 'Tour Du Lịch Nhat Ban Update',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
+    },
+  ]
   return (
     <div>
       <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
@@ -9,6 +36,33 @@ function App() {
         sàng tạo ra những trải nghiệm độc đáo và không quên cho du khách, giúp
         mang đến những chuyến hành trình tuyệt vời.
       </p>
+      <div className="flex gap-2">
+        {tours.map(tour => (
+          <div>
+            <img src={tour.image} width={500} alt="" height={300} />
+            <h2 className="text-3xl font-bold text-orange-300">{tour.title}</h2>
+          </div>
+        ))}
+
+        {/* <div>
+          <img src={tours[0].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[0].title}
+          </h2>
+        </div>
+        <div>
+          <img src={tours[1].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[1].title}
+          </h2>
+        </div>
+        <div>
+          <img src={tours[2].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[2].title}
+          </h2>
+        </div> */}
+      </div>
     </div>
   )
 }
