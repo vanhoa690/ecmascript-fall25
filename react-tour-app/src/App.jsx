77b59cc46372a214b62ccb9788aba39dcb5da050
vanhoa690 <vanhoa690@gmail.com>
2025-11-14 15:00:01 +0700

    update app

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -1,27 +1,13 @@
 function App() {
-  const greeting = 'Chào mừng đến với React!'
-  const name = 'Nguyễn Văn A'
-  const age = 20
-
-  const studentId = 'SV001'
-  const major = 'Công nghệ Thông tin'
-
   return (
-    <div className="container">
-      <h1 className="text-3xl font-bold text-red-400">{greeting}</h1>
-      <p>Đây là component đầu tiên của bạn</p>
-      <h1 className="my-2">Xin chào {name}</h1>
-      <p>Tuổi: {age + 1}</p>
-      <p>Là học viên: {true ? 'Có' : 'Không'}</p>
-      <h2>Thông tin sinh viên</h2>
-      <p>
-        <strong>Họ tên:</strong> {name}
-      </p>
-      <p>
-        <strong>Mã SV:</strong> {studentId}
-      </p>
-      <p className="text-blue-500 font-semibold">
-        <strong>Ngành học:</strong> {major}
+    <div>
+      <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
+        Tour Nội Địa
+      </h1>
+      <p className="text-xl font-medium my-2 px-2">
+        Các chuyến đi đồng hành cùng chúng tôi là khoảnh khắc đặc biêt, luôn sẵn
+        sàng tạo ra những trải nghiệm độc đáo và không quên cho du khách, giúp
+        mang đến những chuyến hành trình tuyệt vời.
       </p>
     </div>
   )
