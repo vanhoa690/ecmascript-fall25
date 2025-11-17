0bd63bf9d8ef51a5a5bed0dfa3ea7cdcf2b791d1
vanhoa690 <vanhoa690@gmail.com>
2025-11-17 12:38:53 +0700

    update

--- react-tour-app/src/components/TourCard.jsx
+++ react-tour-app/src/components/TourCard.jsx
@@ -2,13 +2,16 @@
 // {title: tieu de tour,
 // image: hinh anh tour }
 function TourCard(props) {
+  //   const { title, image } = props;  // desctructing
   return (
     <div>
       <img src={props.image} alt={props.title} width={500} />
       <h3 className="font-semibold">{props.title}</h3>
+      {/* goi component Button */}
+      {/* button Xem them */}
+      {/* button Dat tour */}
     </div>
   );
 }
 
 export default TourCard;
-// rcfc: react function component
