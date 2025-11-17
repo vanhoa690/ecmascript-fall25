20d3c0d5133ceda17a376ee11901d5f9cdc33f6d
vanhoa690 <vanhoa690@gmail.com>
2025-11-17 14:14:13 +0700

    update routing

---
+++ react-tour-app/src/pages/About.jsx
@@ -0,0 +1,21 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Giới thiệu</h1>
+      <p>
+        VTourist – Đơn vị chuyên cung cấp các dịch vụ du lịch (Nội địa – MICE –
+        Teambuilding – Quốc tế), Visa & Vé máy bay hàng đầu Việt Nam. VTourist
+        không chỉ dừng lại trong nước mà còn hướng tới những chuyến du lịch mang
+        tầm cỡ quốc tế, tiếp tục phát triển hệ thống phân phối đến hầu hết các
+        quốc gia trọng điểm du lịch trên thế giới nhằm góp phần quảng bá hình
+        ảnh du lịch Việt Nam và thu hút du khách đến với Việt Nam cũng như đưa
+        người Việt đến khắp năm châu ngày càng nhiều hơn nữa. Hãy đến với
+        Vtourist quý khách luôn được đón chào nồng nhiệt và được phục vụ chu đáo
+        bởi một đội ngũ nhân viên đầy nhiệt huyết, năng động, trẻ trung, nhiều
+        năm kinh nghiệm trong lĩnh vực du lịch. Bất kể lựa chọn lối sống của bạn
+        là gì, chỉ cần một cú chạm, chúng tôi sẽ cùng quý khách viết tiếp hành
+        trình!
+      </p>
+    </div>
+  );
+}
