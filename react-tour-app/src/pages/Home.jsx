f438f5a119353bb30d980c357641c4727b898eb6
vanhoa690 <vanhoa690@gmail.com>
2025-11-21 15:49:00 +0700

    refactor

--- react-tour-app/src/pages/Home.jsx
+++ react-tour-app/src/pages/Home.jsx
@@ -1,37 +1,13 @@
 import { useState } from 'react'
 import axios from 'axios'
-
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Heading from '../components/Heading'
 import TourCard from '../components/TourCard'
-import Button from '../components/Button'
 
 function Home() {
   // mock data
   const [tours, setTours] = useState([])
-  const loadData = () => {
-    setTours([
-      {
-        id: 1,
-        title: 'Tour Du Lịch Cuba',
-        image:
-          'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-      },
-      {
-        id: 2,
-        title: 'Tour Du Lịch Hoa Ky',
-        image:
-          'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-      },
-      {
-        id: 3,
-        title: 'Tour Du Lịch Nhat Ban',
-        image:
-          'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
-      },
-    ])
-  }
 
   const getTours = async () => {
     const { data } = await axios.get('http://localhost:3001/tours')
@@ -41,14 +17,7 @@ function Home() {
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Header />
-      <button onClick={loadData} className="bg-amber-300">
-        Load Data
-      </button>
-      <Button label="Load Data Button" onClick={loadData} />
       <main>
-        {/* <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
-          Tour Nội Địa
-        </h1> */}
         <Heading title="Tour Nội Địa" />
         <p className="text-xl font-medium my-2 px-2">
           Các chuyến đi đồng hành cùng chúng tôi là khoảnh khắc đặc biêt, luôn
