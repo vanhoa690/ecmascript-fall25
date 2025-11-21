3adde28347467fdb86b3fb95a990da31c768d60a
vanhoa690 <vanhoa690@gmail.com>
2025-11-21 15:02:50 +0700

    setTours

--- react-tour-app/src/pages/Home.jsx
+++ react-tour-app/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Heading from '../components/Heading'
@@ -5,29 +6,36 @@ import TourCard from '../components/TourCard'
 
 function Home() {
   // mock data
-  const tours = [
-    {
-      id: 1,
-      title: 'Tour Du Lịch Cuba',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-    },
-    {
-      id: 2,
-      title: 'Tour Du Lịch Hoa Ky',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-    },
-    {
-      id: 3,
-      title: 'Tour Du Lịch Nhat Ban',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
-    },
-  ]
+  const [tours, setTours] = useState([])
+  const loadData = () => {
+    setTours([
+      {
+        id: 1,
+        title: 'Tour Du Lịch Cuba',
+        image:
+          'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+      },
+      {
+        id: 2,
+        title: 'Tour Du Lịch Hoa Ky',
+        image:
+          'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+      },
+      {
+        id: 3,
+        title: 'Tour Du Lịch Nhat Ban',
+        image:
+          'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
+      },
+    ])
+  }
+
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Header />
+      <button onClick={loadData} className="bg-amber-300">
+        Load Data
+      </button>
       <main>
         {/* <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
           Tour Nội Địa
