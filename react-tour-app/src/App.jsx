f121ada96d669f10e4530c4075057ca89c498d26
vanhoa690 <vanhoa690@gmail.com>
2025-11-21 12:36:26 +0700

    state

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 function App() {
-  return <div>App</div>;
+  const [isOn, setIsOn] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const handleClick = () => {
+    const newIsOn = !isOn;
+    console.log(newIsOn);
+    setIsOn(newIsOn);
+  };
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <button onClick={handleClick} className="bg-amber-300 px-4">
+        {isOn ? 'Bat' : 'Tat'}
+      </button>
+    </div>
+  );
 }
 
 export default App;
