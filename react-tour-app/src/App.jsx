ef106e1abf53f5ea0618266096d31b11c5867322
vanhoa690 <vanhoa690@gmail.com>
2025-11-21 14:48:35 +0700

    funct co arg

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -4,16 +4,22 @@ import About from './pages/About'
 import { useState } from 'react'
 
 function App() {
-  const [isOn, setIsOn] = useState(true) // kieu du lieu ma JS co, string, number, array, object ....
+  const [isOn, setIsOn] = useState(true) // khai bao state
   const handleClick = () => {
     const newValue = !isOn
     console.log(newValue)
-    setIsOn(newValue)
+    setIsOn(newValue) // cach dung
   }
 
+  const hanleClickArg = isClicked => {
+    setIsOn(isClicked)
+  }
   return (
     <BrowserRouter>
       <button className="bg-amber-300" onClick={handleClick}>
+        Click {isOn ? 'Bat' : 'Tat'}
+      </button>
+      <button className="bg-amber-300" onClick={() => hanleClickArg(!isOn)}>
         Click {isOn ? 'Bat' : 'Tat'}
       </button>
       <Routes>
