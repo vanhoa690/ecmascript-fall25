688ac5d30a770f3e96ecdd2e540bd46809c1b628
vanhoa690 <vanhoa690@gmail.com>
2025-11-21 14:40:25 +0700

    state

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -1,10 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import { useState } from 'react'
 
 function App() {
+  const [isOn, setIsOn] = useState(true) // kieu du lieu ma JS co, string, number, array, object ....
+  const handleClick = () => {
+    const newValue = !isOn
+    console.log(newValue)
+    setIsOn(newValue)
+  }
+
   return (
     <BrowserRouter>
+      <button className="bg-amber-300" onClick={handleClick}>
+        Click {isOn ? 'Bat' : 'Tat'}
+      </button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
