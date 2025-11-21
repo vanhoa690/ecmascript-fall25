41751e7c78aae4187867f66db78d8db9c5011bcf
vanhoa690 <vanhoa690@gmail.com>
2025-11-21 14:17:39 +0700

    add routes

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -1,7 +1,15 @@
-function App() {
-  // / =>  ra Home
-  // /about => ra About
-  return <div>App</div>
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 export default App
