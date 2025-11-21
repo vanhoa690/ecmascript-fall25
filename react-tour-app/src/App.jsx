d8b4c8f601c8f3186410ce6d4f2812b002e92195
vanhoa690 <vanhoa690@gmail.com>
2025-11-21 12:48:56 +0700

    bai tap useSate

--- react-tour-app/src/App.jsx
+++ react-tour-app/src/App.jsx
@@ -1,33 +1,15 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
-  const [isOn, setIsOn] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  // funct no args
-  const handleClick = () => {
-    const newIsOn = !isOn;
-    setIsOn(newIsOn);
-  };
-
-  // funct co args
-  const handleClickArgs = isClicked => {
-    setIsOn(isClicked);
-  };
-
   return (
-    <div className="flex items-center justify-center h-screen flex-col gap-2">
-      <button onClick={handleClick} className="bg-amber-300 px-4">
-        {isOn ? 'Bat' : 'Tat'}
-      </button>
-      <button
-        onClick={() => handleClickArgs(!isOn)}
-        className="bg-amber-300 px-4"
-      >
-        {isOn ? 'Bat' : 'Tat'}
-      </button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
