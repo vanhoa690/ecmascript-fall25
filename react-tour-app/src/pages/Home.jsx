ea502131686c2500a24152162a33fd11929a8ee1
vanhoa690 <vanhoa690@gmail.com>
2025-11-24 12:40:12 +0700

    useEffect

--- react-tour-app/src/pages/Home.jsx
+++ react-tour-app/src/pages/Home.jsx
@@ -1,49 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import TourCard from '../components/TourCard';
-import Button from '../components/Button';
 
 function Home() {
   // mock data
   const [tours, setTours] = useState([]);
 
-  // call api cap nhat state []
-  const handleLoadData = () => {
-    setTours([
-      {
-        id: 1,
-        title: 'TOUR CARAVAN THÚ VỊ TRÊN KHẮP NẺO ĐƯỜNG',
-        image:
-          'https://s3-cmc.travel.com.vn/vtv-image/Images/Advertisings/bn_250630_caravan-2.webp',
-      },
-      {
-        id: 2,
-        title: 'Du lịch thăm thân tại Úc - Mỹ - Canada',
-        image:
-          'https://s3-cmc.travel.com.vn/vtv-image/Images/Advertisings/bn_250528_statue-liberty-large-american-flag-new-york-skyline-background-162323735.webp',
-      },
-      {
-        id: 3,
-        title: 'Tự hào nét Việt - Ưu đãi kích cầu du lịch nội địa',
-        image:
-          'https://s3-cmc.travel.com.vn/vtv-image/Images/Advertisings/bn_240925_KPSP1-tour-noi-dia-kich-cau.jpg',
-      },
-      {
-        id: 4,
-        title: 'Tự hào nét Việt 4',
-        image:
-          'https://s3-cmc.travel.com.vn/vtv-image/Images/Advertisings/bn_240925_KPSP1-tour-noi-dia-kich-cau.jpg',
-      },
-    ]);
-  };
 
+  useEffect(() => {
+    console.log("Chạy 1 lần khi mount");
+    const getTours = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/tours")
+      console.log(res);
+      setTours(res.data)
+    } catch (error) {
+      console.log(error);
+    }
+    }
+    getTours()
+}, []); // chay 1 lan duy nhat khi truyen []
+ 
   // click button => show danh sach tours
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Header />
       {/* <Button label="Load data" onClick={handleLoadData} /> */}
-      <button onClick={handleLoadData}>Load data</button>
       <main>
         <h1 className="text-3xl font-bold text-blue-600 text-center my-4">
           Khám phá sản phẩm ECMA-WD20306
@@ -56,7 +40,7 @@ function Home() {
         </p>
         <div className="flex gap-2 my-4">
           {tours.map(tour => (
-            <TourCard key={tour.id} title={tour.title} image={tour.image} />
+            <TourCard key={tour.id} title={tour.name} image={tour.image} />
           ))}
         </div>
       </main>
