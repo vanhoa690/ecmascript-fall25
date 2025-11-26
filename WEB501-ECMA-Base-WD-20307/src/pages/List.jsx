f97d98622f3a3a31dd22691e66bff21b23a1d311
vanhoa690 <vanhoa690@gmail.com>
2025-11-26 09:22:31 +0700

    update list

--- WEB501-ECMA-Base-WD-20307/src/pages/List.jsx
+++ WEB501-ECMA-Base-WD-20307/src/pages/List.jsx
@@ -1,13 +1,36 @@
+import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
+import axios from 'axios'
 
 function List() {
-  const handleDelete = () => {
-    toast.success('Delete successfull')
+  const [tours, setTours] = useState([])
+
+  useEffect(() => {
+    const getTours = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:3000/tours')
+        setTours(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getTours()
+  }, [])
+  const handleDelete = async id => {
+    if (window.confirm('Delete')) {
+      try {
+        await axios.delete('http://localhost:3000/tours/' + id)
+        setTours(tours.filter(tour => tour.id !== id))
+        toast.success('Delete successfull')
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Danh sách</h1>
-      <button onClick={handleDelete}>Delete</button>
+
       <div className="overflow-x-auto">
         <table className="w-full border border-gray-300 rounded-lg">
           <thead className="bg-gray-100">
@@ -26,27 +49,25 @@ function List() {
           </thead>
 
           <tbody>
-            <tr className="hover:bg-gray-50">
-              <td className="px-4 py-2 border border-gray-300">1</td>
-              <td className="px-4 py-2 border border-gray-300">Mark</td>
-              <td className="px-4 py-2 border border-gray-300">Otto</td>
-              <td className="px-4 py-2 border border-gray-300">@mdo</td>
-            </tr>
-
-            <tr className="hover:bg-gray-50">
-              <td className="px-4 py-2 border border-gray-300">2</td>
-              <td className="px-4 py-2 border border-gray-300">Jacob</td>
-              <td className="px-4 py-2 border border-gray-300">Thornton</td>
-              <td className="px-4 py-2 border border-gray-300">@fat</td>
-            </tr>
-
-            <tr className="hover:bg-gray-50">
-              <td className="px-4 py-2 border border-gray-300">3</td>
-              <td className="px-4 py-2 border border-gray-300" colSpan={2}>
-                Larry the Bird
-              </td>
-              <td className="px-4 py-2 border border-gray-300">@twitter</td>
-            </tr>
+            {tours.map(tour => (
+              <tr key={tour.id} className="hover:bg-gray-50">
+                <td className="px-4 py-2 border border-gray-300">{tour.id}</td>
+                <td className="px-4 py-2 border border-gray-300">
+                  {tour.name}
+                </td>
+                <td className="px-4 py-2 border border-gray-300">
+                  <img src={tour.image} alt="" width={300} />
+                </td>
+                <td className="px-4 py-2 border border-gray-300">
+                  <button
+                    className="bg-red-400 px-2 rounded-2xl text-white font-bold"
+                    onClick={() => handleDelete(tour.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
