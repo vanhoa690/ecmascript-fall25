8f8ee985b3ef14ffd49cc435294f10b8286e135e
vanhoa690 <vanhoa690@gmail.com>
2025-12-01 12:57:57 +0700

    register

---
+++ WEB501-ECMA-Base-WD20306/src/services/tourService.js
@@ -0,0 +1,5 @@
+export const getTours = () => {};
+export const getTour = id => {};
+export const addTour = payload => {};
+export const updateTour = (id, payload) => {};
+export const deleteTour = id => {};
