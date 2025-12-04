ac9c41e0d00406eecc4aeb6a766a29db9082d5eb
vanhoa690 <vanhoa690@gmail.com>
2025-12-04 08:14:29 +0700

    test

--- WEB501-ECMA-Base-WD-20307/src/pages/List.jsx
+++ WEB501-ECMA-Base-WD-20307/src/pages/List.jsx
@@ -1,50 +1,50 @@
 import { useEffect, useState } from 'react'
-import toast from 'react-hot-toast'
 import axios from 'axios'
+import toast from 'react-hot-toast'
 import { Link } from 'react-router-dom'
+
 function ListPage() {
   const [tours, setTours] = useState([])
-
   useEffect(() => {
     const getTours = async () => {
       try {
         const { data } = await axios.get('http://localhost:3000/tours')
         setTours(data)
       } catch (error) {
-        toast.error(error.message)
+        toast.error(error)
       }
     }
     getTours()
   }, [])
 
   const handleDelete = async id => {
-    try {
-      if (confirm('Tao muon xoa tour nay')) {
-        await axios.delete('http://localhost:3000/tours/' + id)
+    if (confirm('Delete')) {
+      try {
+        await axios.delete('http://localhost:30000/tours/' + id)
         setTours(tours.filter(tour => tour.id !== id))
-        toast.success('Ok tao da xoa duoc roi')
+      } catch (error) {
+        toast.error(error.message) // error: {} -> message
+        console.log(error)
       }
-    } catch (error) {
-      toast.error(error.message) // error {}
     }
   }
-
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Danh sách</h1>
+
       <div className="overflow-x-auto">
         <table className="w-full border border-gray-300 rounded-lg">
           <thead className="bg-gray-100">
             <tr>
-              <th className="px-4 py-2 border border-gray-300 text-left">ID</th>
+              <th className="px-4 py-2 border border-gray-300 text-left">#</th>
               <th className="px-4 py-2 border border-gray-300 text-left">
-                Name
+                First
               </th>
               <th className="px-4 py-2 border border-gray-300 text-left">
-                Last
+                Toogle Active
               </th>
               <th className="px-4 py-2 border border-gray-300 text-left">
-                Handle
+                Actions
               </th>
             </tr>
           </thead>
@@ -53,10 +53,17 @@ function ListPage() {
             {tours.map(tour => (
               <tr key={tour.id} className="hover:bg-gray-50">
                 <td className="px-4 py-2 border border-gray-300">{tour.id}</td>
-                <td className="px-4 py-2 border border-gray-300">
-                  {tour.name}
+                <td className="px-4 py-2 border border-gray-300">Mark</td>
+                <td className="px-4 py-2 border border-gray-300">Otto</td>
+                <td>
+                  <label class="inline-flex items-center cursor-pointer">
+                    <input type="checkbox" class="sr-only peer" id="switch1" />
+                    <div class="w-11 h-6 bg-gray-300 peer-checked:bg-blue-600 rounded-full peer-focus:ring-2 peer-focus:ring-blue-300 transition-colors duration-200"></div>
+                    <span class="ml-3 text-sm font-medium text-gray-700">
+                      Bật / Tắt
+                    </span>
+                  </label>
                 </td>
-                <td className="px-4 py-2 border border-gray-300">....</td>
                 <td className="px-4 py-2 border border-gray-300">
                   <div className="flex gap-2">
                     <button onClick={() => handleDelete(tour.id)}>
